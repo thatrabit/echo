636f516bae600bfe03f4fe1d220ad4a4e55b6381
rabit <38093731+thatrabit@users.noreply.github.com>
2018-07-26 20:55:00 -0400

    .

--- src/agent.cpp
+++ src/agent.cpp
@@ -1,4 +1,24 @@
 #include "agent.hpp"
 
-Agent::Agent(QObject* parent) : QObject(parent) {
+Agent::Agent(QObject* parent) : AgentSimpleSource(parent) {
+    stateChangeTimer = new QTimer(this);
+    connect(stateChangeTimer, SIGNAL(timeout()), this, SLOT(timeout_slot()));
+    stateChangeTimer->start(2000);
+    qDebug() << "source node started";
+}
+
+Agent::~Agent() {
+    stateChangeTimer->stop();
+}
+
+void Agent::server_slot(bool clientState) {
+    qDebug() << "replica state: " << clientState;
+}
+
+void Agent::timeout_slot() {
+    if (currState())
+        setCurrState(false);
+    else
+        setCurrState(true);
+    qDebug() << "source state: " << currState();
 }
--- src/agent.hpp
+++ src/agent.hpp
@@ -6,13 +6,19 @@
 #include "market.hpp"
 #include "order.hpp"
 
-class Agent : public QObject {
+#include "rep_agent_merged.h"
+
+class Agent : public AgentSimpleSource {
     Q_OBJECT
 public:
     explicit Agent(QObject* parent = nullptr);
+    ~Agent();
+    virtual void server_slot(bool clientState);
 public slots:
+    void timeout_slot();
 signals:
 private:
+    QTimer* stateChangeTimer;
 private slots:
 };
 
